ca19ea5ad8f6e283c54402d1e7c8788eeb9bbbdd
0xABDF <abdoatef.ab@gmail.com>
2024-09-03 12:12:38 +0300

    Restructure project files: move .c files to src/ and .h files to include/

--- compiler.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMPILER_H
-#define COMPILER_H
-
-#include "chunk.h"
-
-bool compile(const char *source, Chunk *chunk);
-
-#endif // COMPILER_H
--- debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEBUG_H
-#define DEBUG_H
-
-#include "chunk.h"
-
-void disassembleChunk(Chunk *, const char *);
-int disassembleInstr(Chunk *, int);
-
-#endif // DEBUG_H
